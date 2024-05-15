0e226adcd567e2ecf21bed9762760d50eab1a851
vikash mishra <vikashmishra8371@gmail.com>
2024-05-15 18:43:48 +0530

    ls

--- frontend/src/components/work/Main.jsx
+++ frontend/src/components/work/Main.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { getItem } from "../services/Api";
 import { MdOutlineDoNotDisturb } from "react-icons/md";
-import pdfImg from "../../assets/pdf.png";
+import pdfImg from '../../assets/pdf.png'
 import "./style.css";
 import { Loader } from "../Loader";
 
@@ -10,13 +10,14 @@ const Main = () => {
   const [query, setQuery] = useState(null);
   const [pdf, setPdf] = useState([]);
   const [pdfData, setPdfData] = useState([]);
-  const [filteredData, setFilter] = useState(null);
-  const [change, setChange] = useState(false);
-  const [active, setActive] = useState("All");
-  const [loader, setLoader] = useState(false);
-
+  const [filteredData,setFilter] = useState(null);
+  const [change,setChange] = useState(false);
+  const [active,setActive] = useState("All");
+  const [loader,setLoader] = useState(false);
+  
   const location = useLocation();
 
+  
   const getQuery = () => {
     const searchParams = new URLSearchParams(location.search);
     const queryParamValue = searchParams.get("q");
@@ -26,8 +27,9 @@ const Main = () => {
   const getData = async () => {
     setLoader(true);
     const res = await getItem(query);
-    setLoader(false);
-    if (res && res.pdf) {
+    setLoader(false)
+    if(res&&res.pdf){
+
       setPdf(res.pdf);
     }
   };
@@ -40,113 +42,80 @@ const Main = () => {
   useEffect(() => {
     const fetchPdfData = async () => {
       const pdfDataPromises = pdf.map(async (item) => {
+       console.log(item.name)
         const response = await fetch(
-          `https://colleges-notes-websites.vercel.app/src/uploads/${item.name}`
+          `https://colleges-notes-websites.vercel.app/uploads/${item.name}`
         );
-
+     
         const data = await response.blob();
-        const contentType = "application/pdf";
-        return { ...item, data, contentType };
+        
+        return { ...item, data };
       });
-      setLoader(true);
+      setLoader(true)
       const resolvedPdfData = await Promise.all(pdfDataPromises);
-      console.log(resolvedPdfData);
-      setLoader(false);
+      console.log(resolvedPdfData)
+      setLoader(false)
       setPdfData(resolvedPdfData);
     };
 
     if (pdf.length > 0) {
-      fetchPdfData();
+    fetchPdfData()
+      
     }
   }, [pdf]);
-  useEffect(() => {
-    setFilter(pdfData);
-  }, [pdfData, change]);
-  const handleSort = (val) => {
-    const filteredData = pdfData.filter((data) => {
-      return data.year && data.year == val;
-    });
-    setActive(val);
-    setFilter(filteredData);
-  };
+  useEffect(()=>{
+    setFilter(pdfData)
+  },[pdfData,change])
+const handleSort=(val)=>{
+  const filteredData = pdfData.filter((data)=> {return data.year&&data.year==val});
+  setActive(val)
+ setFilter(filteredData)
+}
   return (
     <div style={{ paddingTop: "20px" }}>
-      {loader && <Loader />}
+      {
+        loader&&<Loader/>
+      }
       <div className="top">
         <h1>{query}</h1>
         <ul>
-          <li
-            className={active == "All" ? "active" : ""}
-            onClick={() => {
-              setChange(!change);
-              setActive("All");
-            }}
-          >
-            All
-          </li>
-          <li
-            className={active == "1" ? "active" : ""}
-            onClick={() => handleSort(1)}
-          >
-            1
-          </li>
-          <li
-            className={active == "2" ? "active" : ""}
-            onClick={() => handleSort(2)}
-          >
-            2
-          </li>
-          <li
-            className={active == "3" ? "active" : ""}
-            onClick={() => handleSort(3)}
-          >
-            3
-          </li>
-          <li
-            className={active == "4" ? "active" : ""}
-            onClick={() => handleSort(4)}
-          >
-            4
-          </li>
+          <li className={active=='All'?'active':''} onClick={()=>{setChange(!change);setActive("All")}}>All</li>
+          <li className={active=='1'?'active':''} onClick={()=>handleSort(1)}>1</li>
+          <li className={active=='2'?'active':''}  onClick={()=>handleSort(2)} >2</li>
+          <li className={active=='3'?'active':''}  onClick={()=>handleSort(3)} >3</li>
+          <li className={active=='4'?'active':''} onClick={()=>handleSort(4)} >4</li>
         </ul>
       </div>
       <section className="card-container">
-        {filteredData && filteredData.length === 0 && (
-          <div style={{ gap: "5px", display: "flex", alignItems: "center" }}>
-            <div>
-              <MdOutlineDoNotDisturb size={29} />
-            </div>
-            <h2>Not Found</h2>
+      {
+        filteredData&&filteredData.length===0&&<div style={{gap:'5px',display:'flex',alignItems:'center'}}>
+          <div>
+          <MdOutlineDoNotDisturb size={29} />
           </div>
-        )}
-        {filteredData &&
-          filteredData.map((item, index) => (
-            <div key={index} className="card">
-              <div className="badge">{item.category}</div>
-              <img src={pdfImg} alt="img" />
+            <h2>Not Found</h2>
+        </div>
+      }
+        {filteredData&&filteredData.map((item, index) => (
+          <div key={index} className="card">
+            <div className="badge">{item.category}</div>
+            <img src={pdfImg} alt="img" />
 
-              <div className="card-body">
-                <h2>{item.code}</h2>
-                <p>{item.subjectName}</p>
+            <div className="card-body">
+              <h2>{item.code}</h2>
+              <p>{item.subjectName}</p>
 
-                <a
-                  href={
-                    item.data &&
-                    URL.createObjectURL(
-                      new Blob([item.data], { type: "application/pdf" })
-                    )
-                  }
-                  download={`file_${index}.pdf`}
-                  className="btn btn-primary"
-                  onClick={() => URL.revokeObjectURL(item.data)} // Revoke blob URL after download
-                >
-                  Download
-                </a>
-              </div>
+              <a
+  href={item.data && URL.createObjectURL(new Blob([item.data], { type: 'application/pdf' }))}
+  download={`file_${index}.pdf`}
+  className="btn btn-primary"
+>
+  Download
+</a>
             </div>
-          ))}
+          </div>
+        ))}
       </section>
-      {/* {   active=='All'&&<div style={{margin:'auto',width:'fit-content',paddingTop:'10px'}}>
+ {/* {   active=='All'&&<div style={{margin:'auto',width:'fit-content',paddingTop:'10px'}}>
         <Link style={{
          borderRadius:'5px',
           fontSize:'22px',
