7be822094407b67dcf233b5226285c3b07f95d2f
vikash mishra <vikashmishra8371@gmail.com>
2024-08-03 14:52:06 +0530

    ls

--- backend/index.js
+++ backend/index.js
@@ -15,16 +15,17 @@ const app = express();
 const PORT = 5000;
 
 // Connect to MongoDB
-app.use(cors({
-  origin:['http://localhost:5173','https://colleges-notes-websites.vercel.app'],
-  methods:["POST","GET"],
-  credentials:true
-}));
 // app.use(cors({
-//   origin:['https://colleges-notes-websites.vercel.app'],
+//   origin:['http://localhost:5173'],
 //   methods:["POST","GET"],
 //   credentials:true
 // }));
+app.use(cors({
+  origin:['https://colleges-notes-websites.vercel.app'],
+  methods:["POST","GET"],
+  credentials:true,
+  optionsSuccessStatus: 200,
+}));
 app.use(cookieParser())
 app.use(express.json());
 dbConnection();
